2f5f19fac588cdcd6f78c1329bda340c6bf9fb6d
unknown <jordangeltner2014@u.northwestern.edu>
2013-05-13 16:07:48 -0500

    updating timer again ;)

--- DownBall/Assets/Timer.cs
+++ DownBall/Assets/Timer.cs
@@ -3,13 +3,19 @@ using System.Collections;
 
 public class Timer : MonoBehaviour {
 	public int myFontSize;
+	public float maxTime;
 	void Start(){
 		myFontSize = 60;
+		maxTime = 10F;
 	}
 	
 	void OnGUI () {
 		GUI.skin.label.fontSize = myFontSize;
-		GUI.Label (new Rect (0,0,200,200), Time.time.ToString().Substring(0,4));
+		if (Time.time > maxTime){
+			GUI.Label (new Rect (0,0,Screen.width,200), "You didn't finish in time!");
+		}
+		else
+			GUI.Label (new Rect (0,0,200,200), Time.time.ToString().Substring(0,4));
 	}
 	
     void Update(){ 
