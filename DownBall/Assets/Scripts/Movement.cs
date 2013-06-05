943f995dcb657bb10468cffe550c33dc6c43dd40
jeanniet13 <jeanniet13@gmail.com>
2013-06-05 15:16:32 -0500

    saving minor changes

--- DownBall/Assets/Scripts/Movement.cs
+++ DownBall/Assets/Scripts/Movement.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Movement : MonoBehaviour {
 
-    public int navForce;
+    public int navForce=500;
 	public int fwdForce=10;
 	public int breakForce=100;
 	public Vector3 jumpVelocity = new Vector3(0,15,5);
@@ -18,15 +18,15 @@ public class Movement : MonoBehaviour {
 		
 		//Left Key
         if (Input.GetKey(KeyCode.LeftArrow) && IsGrounded) {
-			for (navForce=500; navForce>0; navForce -= 10)	{
-				rigidbody.AddForce(-Vector3.right*navForce*Time.deltaTime);
+			for (int i = navForce; i>0; i -= 10)	{
+				rigidbody.AddForce(-Vector3.right*i*Time.deltaTime);
 			}
 		}
 	
 		//Right Key
         if (Input.GetKey(KeyCode.RightArrow) && IsGrounded) {
-			for (navForce=500; navForce>0; navForce -= 10)	{
-            	rigidbody.AddForce(Vector3.right*navForce*Time.deltaTime);
+			for (int i = navForce; i>0; i -= 10)	{
+            	rigidbody.AddForce(Vector3.right*i*Time.deltaTime);
 			}
         }
 		
@@ -41,9 +41,6 @@ public class Movement : MonoBehaviour {
         if (Input.GetKey(KeyCode.UpArrow)) {
             if (rigidbody.velocity.z > 0 && IsGrounded==false) {
                 rigidbody.AddForce(Vector3.forward*0);
-				//rigidbody.velocity = new Vector3(0,15,0);
-				//rigidbody.AddForce(Vector3.forward*weakForce);
-				//rigidbody.AddForce(-Vector3.forward*(weakForce-5));
             }
 			else if (rigidbody.velocity.z > 0  && IsGrounded) {
 				rigidbody.AddForce(Vector3.forward*fwdForce);
@@ -52,8 +49,6 @@ public class Movement : MonoBehaviour {
 		
 		if (Input.GetKey(KeyCode.Space)) {
 			if (IsGrounded){
-				//rigidbody.AddForce(Vector3.up*jumpHeight,ForceMode.Impulse);
-				//rigidbody.AddForce(Vector3.up*jumpForce,ForceMode.Impulse);
 				rigidbody.AddForce(jumpVelocity,ForceMode.VelocityChange);
 			}
 		}
